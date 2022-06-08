7f0ad02c53aa5a40f6d52f15069cc7a63f8fc9dc
Artem Chernii <artem.chernii@osf.digital>
2022-06-08 23:10:44 +0100

    feat: add js login for scrolling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
-window.onload = (e) => {
-  console.log('Window loaded', e);
+const ZSPACING = -1000;
+const frames = [...document.getElementsByClassName('frame')];
+let lastPos = ZSPACING / 5;
+let zVals = [];
+for (let i = 0; i < frames.length; i++) {
+  zVals[i] = i * ZSPACING + ZSPACING;
+}
 
-  window.onscroll = () => {
-    console.log(document);
-  };
+window.onscroll = function () {
+  let top = document.documentElement.scrollTop;
+  let delta = lastPos - top;
+  lastPos = top;
+
+  zVals = zVals.map((val, i) => {
+    const transform = `translateZ(${val}px)`,
+      opacity = val < Math.abs(ZSPACING) / 1.8 ? 1 : 0;
+    frames[i].setAttribute(
+      'style',
+      `transform: ${transform}; opacity: ${opacity}`
+    );
+    return (val += delta * -5.5);
+  });
 };
+window.scroll(0, 1);
